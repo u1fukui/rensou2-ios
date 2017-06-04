eaec10c4e2c3f1ccc667c165f9c2d52e5ef78edb
u1 <u1.kobayashi@gmail.com>
2017-06-05 01:17:29 +0900

    Apply question page design.

--- rensou/QuestionViewController.swift
+++ rensou/QuestionViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+ 
+    @IBOutlet weak var themeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        themeLabel.text = "バナナ"
     }
 
     override func didReceiveMemoryWarning() {
