857fac009645039fd40699b118e5faf9cdba208c
u1 <u1.kobayashi@gmail.com>
2017-07-09 15:09:27 +0900

    Add ad banner to ResultViewController.

--- rensou/ResultViewController.swift
+++ rensou/ResultViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var gadBannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initBannerView()
     }
 
+    func initBannerView() {
+        let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        gadBannerView.adUnitID = appDelegate.getConfigValue(key: "AD_UNIT_ID_FOR_BANNER") as? String
+        gadBannerView.rootViewController = self
+        
+        
+        let request = GADRequest()
+        if TARGET_OS_SIMULATOR == 1 {
+            request.testDevices = [kGADSimulatorID]
+        }
+        gadBannerView.load(request)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
